09239bdaf07657b6a93f9af5a90b6c38e12da8ff
GarethWoodman <gareth.woodman92@gmail.com>
2020-09-29 14:33:37 +0100

    Tested percentage on voucher

--- lib/voucher.rb
+++ lib/voucher.rb
@@ -1,2 +1,7 @@
 class Voucher
+  attr_reader :percentage
+  
+  def initialize(percentage)
+    @percentage = percentage
+  end
 end
--- spec/voucher_spec.rb
+++ spec/voucher_spec.rb
@@ -2,4 +2,13 @@ require './lib/voucher'
 require 'date'
 
 describe 'Voucher' do
+  before :each do
+    @percentage = 10
+  end
+
+  subject { Voucher.new(10)}
+
+  it "prints percentage" do
+    expect(subject.percentage).to eq 10
+  end
 end
